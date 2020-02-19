80fbef242673751b455dc4955ffc47f914d7cfc8
Pontus Tahir <pontus.tahir@gmail.com>
2020-02-19 10:43:47 +0100

    onsdags commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # moment
 
 tesing the framework Moment
+
+### setup
+
+put this in the HTML file:
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.27/moment-timezone-with-data.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.27/moment-timezone-utils.js"></script>
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function currentTime() {
     let rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
     rightNow = updateTime(rightNow);
-    document.getElementById('clock').innerText = rightNow
-    let t = setTimeout(currentTime, 1000); /* setting timer */
+    document.getElementById('clock').innerText = rightNow;
+    let t = setTimeout(currentTime, 1000);
 }
 
 function updateTime(k) {
@@ -16,3 +16,18 @@ function updateTime(k) {
 }
 
 currentTime();
+
+
+function newYork() {
+    var a = moment("2013-11-18 11:55").tz("Asia/Taipei");
+    var b = moment("2013-11-18 11:55").tz("America/Toronto");
+       
+    a.format(); // 2013-11-18T19:55:00+08:00
+    b.format(); // 2013-11-18T06:55:00-05:00
+    
+    a.utc().format(); // 2013-11-18T11:55Z
+    b.utc().format(); // 2013-11-18T11:55Z
+    
+}
+
+newYork()
